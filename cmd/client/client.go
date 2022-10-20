55c148ba120c05314b4c6a99bb7f6afb71c820b6
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-19 21:04:01 -0300

    feat: criando client streaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/batistondeoliveira/fullcycle_comunicacao_gRPC/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	//AddUser(client)
-	AddUserVerboso(client)
+	//AddUserVerboso(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -57,4 +59,50 @@ func AddUserVerboso(client pb.UserServiceClient) {
 		}
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "e1",
+			Name:  "Eliel 1",
+			Email: "eli1@eli.com",
+		},
+		&pb.User{
+			Id:    "e2",
+			Name:  "Eliel 2",
+			Email: "eli2@eli.com",
+		},
+		&pb.User{
+			Id:    "e3",
+			Name:  "Eliel 3",
+			Email: "eli3@eli.com",
+		},
+		&pb.User{
+			Id:    "e4",
+			Name:  "Eliel 4",
+			Email: "eli4@eli.com",
+		},
+		&pb.User{
+			Id:    "e5",
+			Name:  "Eliel 5",
+			Email: "eli5@eli.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+	fmt.Println(res)
 }
